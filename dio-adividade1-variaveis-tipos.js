aed2d9f74e335bbc55abd5afb6df16abd0ddb3e6
holivane <holivane@gmail.com>
2022-08-30 11:35:37 -0300

    atividade palindromos

---
+++ dio-adividade1-variaveis-tipos.js
@@ -0,0 +1,22 @@
+function verificaPalindromoPalavra(stringParaVerificar) {
+  if (!stringParaVerificar) {
+    return "Envie uma ";
+  }
+  let stringRevertida = stringParaVerificar
+    .split("")
+    .reverse()
+    .join("")
+    .replace(/ /g, "");
+  let stringVerificada = stringParaVerificar.replace(/ /g, "");
+  if (stringVerificada == stringRevertida) {
+    return "É palindromo";
+  } else {
+    return "Não é palindromo";
+  }
+}
+
+console.log(verificaPalindromoPalavra("ovo"));
+console.log(verificaPalindromoPalavra("roma me tem amor"));
+console.log(verificaPalindromoPalavra("gato"));
+console.log(verificaPalindromoPalavra("ovo e ovo"));
+
