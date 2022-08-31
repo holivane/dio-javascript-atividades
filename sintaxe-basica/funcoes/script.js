155bddc41498ff0c8f4eb267bb87f526c7894479
holivane <holivane@gmail.com>
2022-08-31 15:41:29 -0300

    Conhecendo mais sobre funções

---
+++ sintaxe-basica/funcoes/script.js
@@ -0,0 +1,15 @@
+// Funções //
+
+function saudacao() {
+  console.log(`Tudo certo!
+Essa mensagem veio de uma função`);
+}
+
+saudacao();
+
+
+function funcaoComParametro(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+
+funcaoComParametro("Holivane")
